d1d14c761493e209ae13c75324a2021e00ca7862
Iroh <stasiron9102@gmail.com>
2026-06-27 22:05:53 +0200

    fix bugs

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
   ],
   nitro: {
     preset: vercelPreset ? "vercel" : undefined,
+    traceDeps: ["tslib*"],
     runtimeConfig: {
       googleCalendarIcalUrl: "",
     },
